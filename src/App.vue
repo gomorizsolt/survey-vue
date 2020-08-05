042baa23207de6b52d4e04f86d5a4a05f8d3d410
Zsolt Gömöri <zsgomoridev@icloud.com>
2020-08-05 13:23:46 +0200

    Initial commit, add Antd

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="./assets/logo.png" />
+    <div>
+      <p>
+        If Ant-Design-Vue is successfully added to this project, you'll see an
+        <code v-text="'<a-button>'"></code>
+        below
+      </p>
+      <a-button type="primary">Primary</a-button>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
